71125c7925f10d32328a4a3663c1389377e3c2db
Mehmetbeydiyceksiniz63 <149189769+Mehmetbeydiyceksiniz63@users.noreply.github.com>
2024-06-26 16:05:39 +0300

    Update song.py

--- JarvisRobo/modules/song.py
+++ JarvisRobo/modules/song.py
@@ -47,7 +47,7 @@ def song(client, message):
         )
         print(str(e))
         return
-    m.edit("» İndiriliyo...\n\nlütfen bekleyin"
+    m.edit("» İndiriliyo...\n\nlütfen bekleyin") 
         with yt_dlp.YoutubeDL(ydl_opts) as ydl:
             info_dict = ydl.extract_info(link, download=False)
             audio_file = ydl.prepare_filename(info_dict)
