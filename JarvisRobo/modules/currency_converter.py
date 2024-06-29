80503dbd0cbf4a2b36dad8f31f3d8e3413ff57bb
Mehmetbeydiyceksiniz63 <149189769+Mehmetbeydiyceksiniz63@users.noreply.github.com>
2024-06-29 11:21:54 +0300

    Update currency_converter.py

--- JarvisRobo/modules/currency_converter.py
+++ JarvisRobo/modules/currency_converter.py
@@ -1,66 +1,66 @@
-içe aktarma istekleri
-telegramdan içe aktarma ParseMode, Güncelleme
-telegram.ext'ten içe aktarma CallbackContext, CommandHandler
+import requests
+from telegram import ParseMode, Update
+from telegram.ext import CallbackContext, CommandHandler
 
-JarvisRobo'dan içe aktar CASH_API_KEY, dağıtıcı
+from JarvisRobo import CASH_API_KEY, dispatcher
 
 
-def dönüştürme(güncelleme: Güncelleme, bağlam: CallbackContext):
-args = update.active_message.text.split(" ")
+def convert(update: Update, context: CallbackContext):
+    args = update.effective_message.text.split(" ")
 
-if len(args) == 4:
-deneyin:
-orig_cur_amount = float(args[1])
+    if len(args) == 4:
+        try:
+            orig_cur_amount = float(args[1])
 
-hariç ValueError:
-update.active_message.reply_text("Geçersiz Para Birimi Tutarı")
-return
+        except ValueError:
+            update.effective_message.reply_text("Invalid Amount Of Currency")
+            return
 
-orig_cur = args[2].upper()
+        orig_cur = args[2].upper()
 
-new_cur = args[3].upper()
+        new_cur = args[3].upper()
 
-request_url = (
-f"https://www.alphavantage.co/query"
-f"?function=CURRENCY_EXCHANGE_RATE"
-f"&from_currency={orig_cur}"
-f"&to_currency={new_cur }"
-f"&apikey={CASH_API_KEY}"
-)
-yanıt = request.get(request_url).json()
-try:
-current_rate = float(
-Response["Gerçek Zamanlı Döviz Değişimi) Rate"]["5. Döviz Kuru"]
-)
-hariç KeyError:
-update.active_message.reply_text("Para Birimi Desteklenmiyor.")
-return
-new_cur_amount = round(orig_cur_amount * current_rate, 5)
-update.active_message.reply_text(
-f"{orig_cur_amount} {orig_cur} = {new_cur_amount} {new_cur}"
-)
+        request_url = (
+            f"https://www.alphavantage.co/query"
+            f"?function=CURRENCY_EXCHANGE_RATE"
+            f"&from_currency={orig_cur}"
+            f"&to_currency={new_cur}"
+            f"&apikey={CASH_API_KEY}"
+        )
+        response = requests.get(request_url).json()
+        try:
+            current_rate = float(
+                response["Realtime Currency Exchange Rate"]["5. Exchange Rate"]
+            )
+        except KeyError:
+            update.effective_message.reply_text("Currency Not Supported.")
+            return
+        new_cur_amount = round(orig_cur_amount * current_rate, 5)
+        update.effective_message.reply_text(
+            f"{orig_cur_amount} {orig_cur} = {new_cur_amount} {new_cur}"
+        )
 
-elif len(args) == 1:
-update.active_message.reply_text(__help__ , parse_mode=ParseMode.MARKDOWN)
+    elif len(args) == 1:
+        update.effective_message.reply_text(__help__, parse_mode=ParseMode.MARKDOWN)
 
-else:
-update.active_message.reply_text(
-f"*Geçersiz Args!!:* Gerekli 3 Ancak Geçildi {len(args) 1}",
-parse_mode=ParseMode .MARKDOWN,
-)
+    else:
+        update.effective_message.reply_text(
+            f"*Invalid Args!!:* Required 3 But Passed {len(args) -1}",
+            parse_mode=ParseMode.MARKDOWN,
+        )
 
 
 __help__ = """
-Parayı bir borsadan diğerine dönüştürür
+Converts money from one exchange to another
 
-Kullanım: /cash tutarı -den
-Örnek: /cash 2 USD INR
-"" "
+Usage: /cash amount from to
+Example: /cash 20 USD INR
+"""
 
-CONVERTER_HANDLER = CommandHandler("nakit", dönüştürme, run_async=True)
+CONVERTER_HANDLER = CommandHandler("cash", convert, run_async=True)
 dispatcher.add_handler(CONVERTER_HANDLER)
-__command_list__ = ["nakit"]
+__command_list__ = ["cash"]
 
 __handlers__ = [CONVERTER_HANDLER]
 
-__mod_name__ = "🔱nakit🔱"
+__mod_name__ = "Cᴀsʜ"
