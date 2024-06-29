2d07054eb9f00ac14d72c0f9ae7a9121b0885b80
Mehmetbeydiyceksiniz63 <149189769+Mehmetbeydiyceksiniz63@users.noreply.github.com>
2024-06-29 10:19:44 +0300

    Update whois.py

--- JarvisRobo/modules/whois.py
+++ JarvisRobo/modules/whois.py
@@ -21,10 +21,10 @@ def ReplyCheck(message: Message):
 
 infotext = (
     "[{full_name}](tg://user?id={user_id})\n\n"
-    " ➻ ᴜsᴇʀ ɪᴅ: `{user_id}`\n"
-    " ➻ ғɪʀsᴛ ɴᴀᴍᴇ: `{first_name}`\n"
-    " ➻ ʟᴀsᴛ ɴᴀᴍᴇ: `{last_name}`\n"
-    " ➻ ᴜsᴇʀɴᴀᴍᴇ: `@{username}`\n"
+    " ➻ kullanıcı id: `{user_id}`\n"
+    " ➻ ilk adı: `{first_name}`\n"
+    " ➻ soy adı: `{last_name}`\n"
+    " ➻ kullanıcı adı: `@{username}`\n"
     " ➻ ʟᴀsᴛ sᴇᴇɴ: `{last_online}`"
 )
 
